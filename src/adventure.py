cd824427ac45d18de0f8649cd77bdcf7267749be
Kyle Tan <tan.kyleemmnuel@gmail.com>
2020-08-05 13:08:31 +0800

    new files

--- src/adventure.py
+++ src/adventure.py
@@ -5,12 +5,13 @@ locations = {0: "You are sitting in front of a computer learning Python",
              4: "You are in a valley beside a stream",
              5: "You are in the forest"}
 
-exits = [{"Q": 0},
-         {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
-         {"N": 5, "Q": 0},
-         {"W": 1, "Q": 0},
-         {"N": 1, "W": 2, "Q": 0},
-         {"W": 2, "S": 1, "Q": 0}]
+exits =  {0: {"Q": 0},
+          1: {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
+          2: {"N": 5, "Q": 0},
+          3: {"W": 1, "Q": 0},
+          4: {"N": 1, "W": 2, "Q": 0},
+          5: {"W": 2, "S": 1, "Q": 0}}
+
 
 loc = 1
 while True:
@@ -20,10 +21,8 @@ while True:
     if loc == 0:
         break
 
-    goto = input('You can go to ' + availableExits).upper()
-    print(exits[loc])
+    goto = input('You can go to ' + availableExits + ": ").upper()
     if goto in exits[loc]:
         loc = exits[loc][goto]
-        print(loc)
     else:
         print('You cannot go in that direction')
